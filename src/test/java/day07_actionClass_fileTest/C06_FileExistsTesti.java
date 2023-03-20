3b5ac8aeaa3cd8eac46af3f9d723e250d6eb075b
Sevilay <you@example.com>
2023-03-21 00:33:22 +0200

    son

--- src/test/java/day07_actionClass_fileTest/C06_FileExistsTesti.java
+++ src/test/java/day07_actionClass_fileTest/C06_FileExistsTesti.java
@@ -10,17 +10,34 @@ import java.nio.file.Paths;
 
 public class C06_FileExistsTesti extends TestBase {
     @Test
-    public void test(){
+    public void test() {
         //2. https://the-internet.herokuapp.com/download adresine gidelim.
         driver.get("https://the-internet.herokuapp.com/download");
         //3. logo.png dosyasını indirelim
-        driver.findElement(By.linkText("logo.png")).click();
+        driver.findElement(By.linkText("myfile.png")).click();
         bekle(5);
         //4. Dosyanın başarıyla indirilip indirilmediğini test edelim
 
-        String dinamikDosyaYolu=System.getProperty("user.home")+"\\Downloads\\logo.png";
+        String dinamikDosyaYolu = "C:\\Users\\sevil\\Downloads\\logo.png";
         Assert.assertTrue(Files.exists(Paths.get(dinamikDosyaYolu)));
-        String hocaDosyaYolu="C:\\Users\\sevil\\Downloads\\TheDelta.docx";
-        Assert.assertTrue(Files.exists(Paths.get(hocaDosyaYolu)));
+ /* String hocaDosyaYolu="C:\\Users\\sevil\\Downloads\\TheDelta.docx";
+        // String dinamikDosyaYolu=System.getProperty("user.home")+"\\Downloads\\logo.png";
+        Assert.assertTrue(Files.exists(Paths.get(hocaDosyaYolu)));*/
+
     }
-}
+@Test
+        public void test2(){
+    String dinamikDosyaYolu = "C:\\Users\\sevil\\Downloads\\myfile (4).png";
+
+    Assert.assertTrue(Files.exists(Paths.get(dinamikDosyaYolu)));
+    //"C:\Users\sevil\Downloads\"
+        }
+
+
+
+
+
+
+        //"C:\Users\sevil\Downloads\myfile (1).png"
+    }
+
--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -5,6 +5,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -12,8 +13,14 @@ public class TestBase {
     public WebDriver driver;
     @Before
     public void setup(){
-        WebDriverManager.chromedriver().setup();
+      /*  WebDriverManager.chromedriver().setup();
         driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));*/
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
